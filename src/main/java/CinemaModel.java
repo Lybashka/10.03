b53a7edc9d8dd5048e396073dd040b69aad6589b
IIS <gui>
2016-03-10 10:50:12 +0300

    Initial commit

---
+++ src/main/java/CinemaModel.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+/**
+ * Created by IIS on 10.03.2016.
+ */
+public interface CinemaModel {
+//    List<seance> find (String str,LocalDate d);
+//    Hall getHall(int seanceId);
+//    boolean reserve(int seanceId,int row,int seat,String userId,LocalDate date);
+}
